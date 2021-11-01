bcd731b0103f8455264008d93b2c001dd532cbc9
taj1-takum1 <takumin390511@gmail.com>
2021-11-01 15:01:23 +0900

    feature search

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[edit show]
-  before_action :move_to_index, except: %i[index show]
+  before_action :move_to_index, except: %i[index show search]
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
   end
@@ -29,6 +29,10 @@ class TweetsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
 
   private
